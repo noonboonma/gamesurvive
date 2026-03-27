50d0b35c10f5831a382a72d8ca7a26e51f5ec300
noon <noongghn@hotmail.com>
2026-03-27 19:20:51 +0700

    fix: pool

--- Backend/server.js
+++ Backend/server.js
@@ -15,11 +15,10 @@ app.use(express.json());
 
 // --- Database Connection ---
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false // จำเป็นสำหรับการเชื่อมต่อฐานข้อมูลภายนอก (เช่น Render)
+    }
 });
 
 pool.connect((err, client, release) => {
